fdc841d3a4df4eadaf5f4c4a69b0011825fad0fa
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-05-10 19:14:48 +0900

    [Start] 4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,8 +12,15 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+  
   def create
     blog = Blog.new(blog_params)
     blog.save
@@ -24,5 +31,5 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
